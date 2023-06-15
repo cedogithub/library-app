d530977ad6a839aaeef7ebdee56755ad50eba502
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-06-15 07:28:00 -0400

    version 2.0: new UI, animations, code logic,json, files upload...

--- README.md
+++ README.md
@@ -1,22 +1,55 @@
-# My Library App (Version 1)
+# celibrary v2 (English)
 
-This is the first version of the My Library app, a simple web application for managing your book library. With this app, you can keep track of your favorite books, including details like the author, title, number of pages, and whether you have read them or not.
+
+CeLibrary v2 is an enhanced version of the library management application designed to provide you with a seamless reading experience. With its intuitive interface, new UI, and stylish design, managing and organizing your book collection has never been easier.
 
 ## Features
 
-- Add new books to your library by filling out a form.
-- Display all the books in your library with their respective details.
-- Mark books as read or unread using a checkbox.
-- Remove books from your library with the click of a button.
+- **Add New Books**: Easily add new books to your library by providing the title, author, number of pages, and reading status. You can also upload a cover image for each book.
+
+- **Update Reading Status**: Track your reading progress with the new reading status feature. Simply toggle the reading status of each book between "Read" and "Not Read" with just one click.
+
+- **Remove Books**: Easily remove unwanted books from your library. Simply click the delete button associated with the book you want to remove.
+
+- **Animated Book Cards**: Enjoy a visually appealing experience with random animations of book cards that bring your library to life.
 
 ## Future Updates
 
-In future versions of the app, I plan to implement the following features and enhancements:
+- **Sorting and Filtering**: Easily sort and filter your book collection based on various criteria such as author, genre, or reading status.
 
-- Responsiveness: Make the app mobile-friendly and adaptable to different screen sizes.
-- Form Validation: Implement client-side form validation to ensure accurate and complete book entries.
-- Local Storage: Save the library data locally so that it persists between page reloads.
-- Book Images: Allow users to add book cover images to enhance visual representation.
-- Additional Features: Explore more features like sorting, searching, and filtering books.
+- **Multiple Libraries**: Create and manage multiple libraries to separate different genres or categories of books.
 
-Stay tuned for updates as I continue to improve the My Library app and provide a better book management experience!
+- **Form Validation**: Implement client-side form validation to ensure accurate and complete book entries.
+
+- **Enhanced Mobile Experience**: Optimize the application for a smooth mobile experience, allowing you to manage your library on the go.
+
+Stay tuned for these exciting updates and make the most out of your reading journey with CeLibrary!
+
+---
+
+# celibrary v2 (French)
+
+
+CeLibrary v2 est une version améliorée de l'application de gestion de bibliothèque conçue pour vous offrir une expérience de lecture fluide. Avec son interface intuitive, son nouveau design et son style élégant, la gestion et l'organisation de votre collection de livres n'ont jamais été aussi faciles.
+
+## Fonctionnalités
+
+- **Ajouter de nouveaux livres**: Ajoutez facilement de nouveaux livres à votre bibliothèque en fournissant le titre, l'auteur, le nombre de pages et l'état de lecture. Vous pouvez également télécharger une image de couverture pour chaque livre.
+
+- **Mettre à jour l'état de lecture**: Suivez votre progression de lecture grâce à la nouvelle fonctionnalité d'état de lecture. Basculez simplement l'état de lecture de chaque livre entre "Lu" et "Non lu" en un seul clic.
+
+- **Supprimer des livres**: Supprimez facilement les livres indésirables de votre bibliothèque. Il vous suffit de cliquer sur le bouton de suppression associé au livre que vous souhaitez supprimer.
+
+- **Cartes animées de livres**: Profitez d'une expérience visuellement attrayante avec des animations aléatoires de cartes de livres qui donnent vie à votre bibliothèque.
+
+## Mises à jour futures
+
+- **Tri et filtrage**: Triez et filtrez facilement votre collection de livres en fonction de différents critères tels que l'auteur, le genre ou l'état de lecture.
+
+- **Bibliothèques multiples**: Créez et gérez plusieurs bibliothèques pour séparer les différents genres ou catégories de livres.
+
+- **Validation de formulaire**: Mettez en place une validation de formulaire côté client pour garantir des entrées de livres précises et complètes.
+
+- **Expérience mobile améliorée**: Optimisez l'application pour une expérience mobile fluide, vous permettant de gérer votre bibliothèque où que vous soyez.
+
+Restez à l'affût de ces mises à jour passionnantes et profitez pleinement de votre voyage de lecture avec CeLibrary!
--- script.js
+++ script.js
@@ -1,102 +1,126 @@
 // Define the Book class to create book objects
 class Book {
-  constructor(author, title, pages, read, id) {
-    this.author = author; // Author of the book
-    this.title = title; // Title of the book
-    this.pages = pages; // Number of pages in the book
-    this.read = read; // Whether the book has been read or not
-    this.id = id; // Unique identifier for the book
+  constructor(author, title, pages, read, id, image) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+    this.image = image;
   }
 }
 
 // Select DOM elements
 const booksContainer = document.querySelector(".books-container"); // Container element to display books
-const form = document.querySelector("form"); // Form element for adding new books
-const newButton = document.querySelector(".new-book"); // Button to toggle visibility of the form
+const newButton = document.querySelector(".new-book"); // Button to open the modal form
+const modal = document.querySelector(".modal"); // Modal element
+const closeButton = document.querySelector(".close"); // Close button inside the modal
+const modalForm = document.querySelector(".modal form"); // Form element inside the modal
 
-// Create initial book objects
-const book1 = new Book("James", "Game of Thrones", 1, true, 291); // Sample book object
-const book2 = new Book("George", "Breaking Bad", 1, true, 129); // Sample book object
-let myLibrary = [book1, book2]; // Array to store the books in the library
+let myLibrary = []; // Array to store the books in the library
 
-// Toggle form visibility when the "New Book" button is clicked
+// Open the modal form when the "New Book" button is clicked
 newButton.addEventListener("click", () => {
-  form.classList.toggle("hidden");
+  modal.style.display = "block";
+});
+
+// Close the modal form when the close button is clicked
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close the modal form when the user clicks outside the modal content
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
 
 // Handle form submission
-form.addEventListener("submit", (e) => {
+modalForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get form input values
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").checked;
+  const read = document.querySelector("#read").value === "true"; // Convert the value to a boolean
   const id = Date.now(); // Generate a unique ID using the current timestamp
 
+  // Get the selected image file if provided
+  const imageInput = document.querySelector("#image");
+  let image = "./book.png"; // Default empty image URL
+  imageInput.classList.add("default-image");
+  if (imageInput.files.length > 0) {
+    const imageFile = imageInput.files[0];
+    image = URL.createObjectURL(imageFile);
+  }
+
   // Add the new book to the library
-  addBookToLibrary(author, title, pages, read, id);
+  addBookToLibrary(author, title, pages, read, id, image);
 
   // Clear the books container and display the updated library
   booksContainer.textContent = "";
   displayBooks(myLibrary);
+
+  // Reset the form
+  modalForm.reset();
+
+  // Close the modal form
+  modal.style.display = "none";
 });
 
 // Display the books in the library
 const displayBooks = (library) => {
   booksContainer.innerHTML = "";
 
-  for (let i = 0; i < library.length; i++) {
-    const book = library[i];
-
+  library.forEach((book, index) => {
     // Create a card element for the book
     const card = document.createElement("div");
     card.classList.add("bookcard");
-  
+
+    // Create an image element for the book cover
+    const coverElement = document.createElement("img");
+    coverElement.classList.add("cover");
+    if (book.image == "./book.png") coverElement.classList.add("default-image");
+    coverElement.src = book.image; // Set the image source dynamically
+    card.appendChild(coverElement);
+
     // Create a title element and set its text content
     const titleElement = document.createElement("h3");
     titleElement.textContent = book.title;
     card.appendChild(titleElement);
-  
+
     // Create an author element and set its text content
     const authorElement = document.createElement("p");
     authorElement.textContent = `Author: ${book.author}`;
     card.appendChild(authorElement);
-  
+
     // Create a pages element and set its text content
     const pagesElement = document.createElement("p");
     pagesElement.textContent = `Pages: ${book.pages}`;
     card.appendChild(pagesElement);
-  
-    // Create a read element and set its text content
-    const readElement = document.createElement("p");
-    readElement.classList.add("read");
-    readElement.textContent = `Read: ${book.read ? "Yes" : "No"}`;
-    card.appendChild(readElement);
-    
-    // Create a checkbox for marking the book as read or unread
-    let read = document.createElement("input");
-    read.type = "checkbox";
-    read.checked = book.read;
 
-    read.addEventListener("change", (e) => {
-      book.read = e.target.checked;
-      // Find the parent book card element with the "closest()" method
-      const bookCard = e.target.closest(".bookcard");
+    // Create a button for marking the book as read or not read
+    const readButton = document.createElement("button");
+    readButton.textContent = book.read ? "Read" : "Not Read";
+    readButton.classList.add("read-button");
+    readButton.classList.add(book.read ? "read" : "not-read");
 
-      // Find the read status element within the current book card
-      const readStatus = bookCard.querySelector(".read");
-      readStatus.textContent = `Read: ${book.read ? "Yes" : "No"}`;
+    readButton.addEventListener("click", () => {
+      book.read = !book.read;
+      readButton.textContent = book.read ? "Read" : "Not Read";
+      readButton.classList.toggle("read");
+      readButton.classList.toggle("not-read");
     });
 
-    card.appendChild(read);
+    card.appendChild(readButton);
 
     // Create a remove button for deleting the book
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.classList.add("remove-button");
-    removeButton.setAttribute("data-id", i);
+    removeButton.setAttribute("data-id", index); // Use index as data-id
     removeButton.addEventListener("click", (e) => {
       const dataID = parseInt(e.target.getAttribute("data-id"));
       const result = myLibrary.filter((book, index) => index !== dataID);
@@ -108,14 +132,41 @@ const displayBooks = (library) => {
 
     // Append the book card to the books container
     booksContainer.appendChild(card);
-  }
+  });
 };
 
 // Add a book to the library
-function addBookToLibrary(author, title, pages, read, id) {
-  const newBook = new Book(author, title, pages, read, id);
+function addBookToLibrary(author, title, pages, read, id, image) {
+  const newBook = new Book(author, title, pages, read, id, image);
   myLibrary.push(newBook);
 }
 
-// Display the initial set of books
-displayBooks(myLibrary);
+// Load the JSON data and process it
+fetch("./books.json")
+  .then((response) => response.json())
+  .then((data) => {
+    myLibrary = data.map((bookData) => {
+      const { author, title, pages, read, id, image } = bookData; // Destructuring
+      return new Book(author, title, pages, read, id, image);
+    });
+
+    // Display the books in the library
+    displayBooks(myLibrary);
+  });
+
+// Function to add the "animate" class to random book cards after a delay
+function animateRandomBookCard() {
+  const bookCards = document.querySelectorAll(".bookcard");
+  const randomBookCard =
+    bookCards[Math.floor(Math.random() * bookCards.length)];
+
+  randomBookCard.classList.add("animate");
+
+  // Remove the "animate" class after a certain duration (e.g., 2 seconds)
+  setTimeout(() => {
+    randomBookCard.classList.remove("animate");
+  }, 2000); // Adjust the duration
+}
+
+// Start the loop to animate random book cards
+setInterval(animateRandomBookCard, 4000); // Adjust the delay between animations as needed
